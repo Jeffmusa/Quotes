34974eece0b36343ac0cf4f9f016aa54101addcb
Jeff Musa <jeffmusa@gmail.com>
2018-08-10 14:30:34 +0300

    input fields added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFormComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes = [
+
+  ]
+
   constructor() { }
 
   ngOnInit() {
