70d42fd1277dc0ac0e077d0e55f4bc95db93c802
Jeff Musa <jeffmusa@gmail.com>
2018-08-12 22:13:43 +0300

    highlight directive added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { HighlightDirective } from './highlight.directive';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { QuoteDetailsComponent } from './quote-details/quote-details.component';
     AppComponent,
     QuoteFormComponent,
     QuoteComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    HighlightDirective
 
   ],
   imports: [
---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive,ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private elem:ElementRef) {
+   this.elem.nativeElement.style.textDecoration='line-through';
+  }
+
+}
